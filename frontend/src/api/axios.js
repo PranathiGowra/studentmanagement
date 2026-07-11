bfa35aa4451ec8964712a873340555ef37f168e6
Pranathi Charani <pranathigowra.charani@gmail.com>
2026-07-11 10:24:45 +0530

    Production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://spring-boot-backend-production-a9be.up.railway.app/api"
 });
 
 API.interceptors.request.use((config) => {
